793be1b3cb3369e9795379231c541d8c188e6369
open-pingwin <40059492+scripchenko@users.noreply.github.com>
2026-09-06 14:57:17 +0300

    chore: bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pingwin.vpn"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
